e0e0c8673a52d563501a66cb5ff41bf7d90c8632
Franco Gudiño <francogudino12@gmail.com>
2024-02-10 16:21:41 -0300

    Mock de productos

---
+++ src/productsMock.js
@@ -0,0 +1,41 @@
+// por lo menos 4 productos y 2 productos de cada categoria
+
+export let products = [
+  {
+    id: 1,
+    title: "titulo",
+    price: 1,
+    description: "asd",
+    image:
+      "https://cloudfront-eu-central-1.images.arcpublishing.com/prisaradiolos40/XVRM4ZSAIRIFVOQ5U5EFOYJ6ZQ.jpg",
+    stock: 1,
+    category: "asd",
+  },
+  {
+    id: 2,
+    title: "titulo",
+    price: 1,
+    description: "asd",
+    image: "url",
+    stock: 1,
+    category: "asd",
+  },
+  {
+    id: 3,
+    title: "titulo",
+    price: 1,
+    description: "asd",
+    image: "url",
+    stock: 1,
+    category: "asd",
+  },
+  {
+    id: 4,
+    title: "titulo",
+    price: 1,
+    description: "asd",
+    image: "url",
+    stock: 1,
+    category: "asd",
+  },
+];
