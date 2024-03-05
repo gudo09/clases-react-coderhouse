4de6fc4e0cd69920d426a7b26e08dc8e36e248a1
Franco Gudiño <francogudino12@gmail.com>
2024-03-05 17:48:24 -0300

    Agrego footer haciendo uso del componente Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { CssBaseline } from "@mui/material";
-import { Navbar } from "./components/layout/Navbar";
+
 import { ItemDetailContainer, ItemListContainer } from "./components/pages";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Cart } from "./components/common";
+import { Layout } from "./components/layout";
+
 
 export const App = () => {
   return (
@@ -10,12 +12,16 @@ export const App = () => {
       <BrowserRouter>
         {/*Reseteo los estilos de CSS con el componente CssBaseline de Material UI*/}
         <CssBaseline />
-        <Navbar />
+
         <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/item/:id" element={<ItemDetailContainer />} />
-          <Route path="/category/:category" element={<ItemListContainer />} />
+          <Route element={<Layout />}>
+            {/* rutas estaticas */}`
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/cart" element={<Cart />} />
+            {/* rutas dinamicas */}
+            <Route path="/item/:id" element={<ItemDetailContainer />} />
+            <Route path="/category/:category" element={<ItemListContainer />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
